d7050b135498d36cec5813da511de356d3c95641
Tre`Von McKay <trevon.mckay@gmail.com>
2016-12-09 02:32:58 -0800

    Corrected Assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Firebase.Portable")]
-[assembly: AssemblyDescription(".NET client for Firebase.io (Portable library)")]
-[assembly: AssemblyProduct("Firebase.Portable")]
-[assembly: AssemblyInformationalVersion("2.0.2-alpha")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: InternalsVisibleTo("FirebaseSharp.Tests")]
+[assembly: AssemblyTitle("FirebaseDotNetCore")]
+[assembly: AssemblyDescription("A .NET Core compatible Firebase.io client")]
+[assembly: AssemblyProduct("FirebaseDotNetCore")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("FirebaseDotNetCore.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
